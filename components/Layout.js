47499df20e188c62e55a347a5be1aa4cfe6295ae
zonoryo17 <p.naka.0p0@gmail.comqpikoro0869pikoro03git config --global core.editor code>
2022-06-21 12:42:21 +0900

    complete deploy v1.0

---
+++ components/Layout.js
@@ -0,0 +1,38 @@
+import Head from "next/head";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+
+const name = "ぞのりょー";
+export const siteTitle = "Next.js blog"; 
+
+function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+          <img src="/images/zonoryo.jpg" className={`${utilStyles.borderCircle} ${styles.headerHomeImage}`}/>
+          <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+          <img src="/images/zonoryo.jpg" className={`${utilStyles.borderCircle}`} width="100px" height="100px"/>
+          <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div>
+          <Link href="/">←　ホームへ戻る</Link>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Layout;
---
+++ pages/index.js
@@ -0,0 +1,62 @@
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Layout, { siteTitle } from '../components/layout'
+
+import Link from 'next/link'
+import utilStyles from '../styles/utils.module.css'
+import { getPostsData } from '../lib/post';
+import Head from 'next/head'
+
+//SSGの場合
+export async function getStaticProps() {
+  const allPostsData = getPostsData(); //mdファイルのid,title,dataなどが格納されるy
+
+  return {
+    props: {
+      allPostsData,
+      //コンポーネントに渡すためのprops
+    },
+  };
+}
+
+// //SSRの場合
+// export async function getServerSideProps(context) {
+//   return {
+//     props: {
+
+//     }
+//   }
+// }
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>現在はリハビリ職をしていて，エンジニアとして転職を目指してプログラミングを学習しています。</p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2>📝エンジニアのブログ</h2>
+        <div className={styles.grid}>
+          {allPostsData.map(({id, title, date, thumbnail}) => (
+            <article key={id}>
+            <Link href={`/posts/${id}`}>
+              <img src={`${thumbnail}`}
+                className={styles.thumbnailImage}/>
+            </Link>
+            <Link href={`/posts/${id}`}>
+              <a className={utilStyles.boldText}>{title}</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>{date}</small>
+          </article>
+          ))}
+        </div>
+      </section>
+
+    </Layout>
+  )
+}
